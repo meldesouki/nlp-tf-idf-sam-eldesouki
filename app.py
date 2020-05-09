2abf2c89dc30d09825064a8a6a5269c6f906e75c
ethansam911 <esam5556@gmail.com>
2020-05-09 18:38:39 -0400

    Added comments to app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import sys
 import re
 import math
 import pyspark
+
 # Local[*] is used to run spark locally with as many worker threads as logical cores on your machine
 # nlp_tf_idf is the name of the application
 #Gateway to pyspark
@@ -51,17 +52,23 @@ if __name__ == '__main__':
 
     doc_count = docs.count()
     output.write(f'Number of documents: {doc_count}\n')
-
+      #Add  a and b
+      #The flatMap() is used to produce multiple output elements for 
+      #each input element. When using map(), the function we provide 
+      #to flatMap() is called individually for each element in our input RDD. 
+      #Instead of returning a single element, an iterator with the return values is returned.
     words_by_doc = docs.flatMap(doc_to_words) \
             .reduceByKey(lambda a, b: a + b) # term count per doc
     tf = words_by_doc.map(lambda word: (word[0][2], [(word[0][0], word[1]/word[0][1])])) \
             .reduceByKey(lambda a, b: a + b)
 
-    # k: word, v: (idf, [(docid, tf)])
+    #Operates on key/value pairs and merges the value for each key
+
+    # key: word, value: (idf, [(docid, tf)])
     tf_idf = tf.map(lambda word: (word[0],
         (math.log(doc_count / len(word[1]), 10), word[1])))
-
-    # k: word, v: [(docid, tf*idf)]
+ 
+    # key: word, value: [(docid, tf*idf)]
     tf_idf_merged = tf_idf.map(lambda word: (word[0], {i[0]: word[1][0] * i[1] for i in word[1][1]}))
 
 
@@ -72,11 +79,11 @@ if __name__ == '__main__':
     q_norm = sum(map(lambda x: x ** 2, q[0].values())) ** (1/2)
 
     similartities = tf_idf_merged.map(lambda w: (w[0], sum([q[0][elem] * w[1][elem] for elem in q[0].keys() & w[1].keys()]) / (sum(map(lambda x: x ** 2, w[1].values())) ** (1/2) * q_norm)))
+      
+    # take top 5 from ordered the pair in descending order
+    terms = similartities.takeOrdered(6, key=lambda word: -word[1])
 
-    # take top 20 from ordered the pair in descending order
-    terms = similartities.takeOrdered(21, key=lambda word: -word[1])
-
-    output.write(f'\nTop 20 similar to {QUERY}:\n')
-    # query should match itself (~1.0 score), so we skip that
+    output.write(f'\nTop 5 similar to {QUERY}:\n')
+    # We skip the query term because that matches itself (~1.0 score)
     output.writelines([f'{word} {item}\n' for (word, item) in terms if word != QUERY])
     output.close()
