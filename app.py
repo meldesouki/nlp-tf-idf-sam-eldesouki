c099213c1de540df22fb835fe05624cbd8602d2c
ethansam911 <esam5556@gmail.com>
2020-05-13 15:41:35 -0400

    Changes to App.py

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def document_to_words(doc):
         ret.append(((doc[0], num_words, word), 1))
     return ret
 
+#Reject # of arguments if not 3
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Incorrect number of arguments. Must have file name and query term.')
@@ -57,17 +58,22 @@ if __name__ == '__main__':
       #each input element. When using map(), the function we provide 
       #to flatMap() is called individually for each element in our input RDD. 
       #Instead of returning a single element, an iterator with the return values is returned.
+   
     words_by_document = documents.flatMap(document_to_words) \
-            .reduceByKey(lambda a, b: a + b) # term count per doc
+            .reduceByKey(lambda a, b: a + b) # Number of terms per document
+
+    # 1 Generate the term frequency - Split the document into words and divide by the total term count 
     tf = words_by_document.map(lambda word: (word[0][2], [(word[0][0], word[1]/word[0][1])])) \
             .reduceByKey(lambda a, b: a + b)
 
     #Operates on key/value pairs and merges the value for each key
 
-    # key: word, value: (idf, [(docid, tf)])
+    # 2 key: word, value: (idf, [(docid, tf)])
     idf = tf.map(lambda word: (word[0],(math.log(document_count / len(word[1]), 10), word[1])))
  
-    # key: word, value: [(docid, tf*idf)]
+    # 3 key: word, value: [(docid, tf*idf)]
+    # score = tf_idf_merged = tf * idf 
+    # score = term frequency * (number of docs/number of docs appeared)
     tf_idf_merged = idf.map(lambda word: (word[0], {i[0]: word[1][0] * i[1] for i in word[1][1]}))
 
 
@@ -76,7 +82,7 @@ if __name__ == '__main__':
 
     q = [i for i in q]
     q_norm = sum(map(lambda x: x ** 2, q[0].values())) ** (1/2)
-
+    #Similarity
     similar = tf_idf.map(lambda w: (w[0], sum([q[0][element] * w[1][element] for element in q[0].keys() & w[1].keys()]) / (sum(map(lambda x: x ** 2, w[1].values())) ** (1/2) * q_norm)))
       
     # take top 5 from ordered the pair in descending order
