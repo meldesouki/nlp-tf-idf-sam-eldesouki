081310d9e50ec874999b1befdbbfeea6ea2abdd2
ethansam911 <esam5556@gmail.com>
2020-05-13 18:03:40 -0400

    Added descriptions on how mapreduce works for TF

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import sys
 import re
 import math
 import pyspark
+
+
+
 # Local[*] is used to run spark locally with as many worker threads as logical cores on your machine
 # nlp_tf_idf is the name of the application
 #Gateway to pyspark
@@ -20,6 +23,7 @@ QUERY = ""
 # Convert every line in the text to a document
 def txt_to_doc(txt):
     #Python verb which splits text into words
+    #If no arguments are given, it splits on repeated runs of whitespace.
     splitted = txt.split()
     # return (document id, words in doc)
     return splitted[0], [w for w in splitted[1:] if DIS_REGEX.match(w) or w == QUERY]
@@ -27,6 +31,8 @@ def txt_to_doc(txt):
 # Split a document into words 
 def doc_to_words(doc):
     words = doc[1]
+
+    #len returns the number of items in an object
     num_words = len(words)
     ret = []
     for word in words:
@@ -35,8 +41,7 @@ def doc_to_words(doc):
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print('Invalid number of arguments, program expects only filename '
-              'relative to `data` directory to analyze and query term.')
+        print('Incorrect number of arguments. Must have file name and query term.')
         exit(0)
 
     filename = sys.argv[1]
@@ -48,36 +53,61 @@ if __name__ == '__main__':
 
     #Get file as input
     txt = sc.textFile(filename)
+    #docs = a tuple of the doc_id and the rest of the string 
+    # apply a function on all the elements of specified iterable 
+    # and return map object. 
+  
+    # docs is a map object is an iterator, so 
+    # we can iterate over its elements. We can also convert map object to sequence objects such as list, tuple
     docs = txt.map(txt_to_doc)
 
+    #The count() method returns the number of occurrences of an element in a list - perform count on map-object
     doc_count = docs.count()
-    output.write(f'Document count: {doc_count}\n')
-
+    output.write(f'Number of documents: {doc_count}\n')
+      #Add  a and b
+      #The flatMap() is used to produce multiple output elements for 
+      #each input element. When using map(), the function we provide 
+      #to flatMap() is called individually for each element in our input RDD. 
+      #Instead of returning a single element, an iterator with the return values is returned.
+      #Flattened into an array - key/value pair turned into an array
     words_by_doc = docs.flatMap(doc_to_words) \
             .reduceByKey(lambda a, b: a + b) # term count per doc
     tf = words_by_doc.map(lambda word: (word[0][2], [(word[0][0], word[1]/word[0][1])])) \
-            .reduceByKey(lambda a, b: a + b)
+            .reduceByKey(lambda a, b: a + b) #tf formula from Lecture 9, slide 64
 
-    # k: word, v: (idf, [(docid, tf)])
+    #Spark RDD reduceByKey function merges the values for each key using an associative 
+    #reduce function. Basically reduceByKey function works only for RDDs which contains key 
+    #and value pairs kind of elements
+
+
+
+    #Operates on key/value pairs and merges the value for each key
+
+    # key: word, value: (idf, [(docid, tf)])
     tf_idf = tf.map(lambda word: (word[0],
-        (math.log(doc_count / len(word[1]), 10), word[1])))
-
-    # k: word, v: [(docid, tf*idf)]
+        (math.log(doc_count / len(word[1]), 10), word[1]))) #idf formula from Lecture
+ 
+    # key: word, value: [(docid, tf*idf)]
+    # Given a key, we score each document using the tf*idf formula
     tf_idf_merged = tf_idf.map(lambda word: (word[0], {i[0]: word[1][0] * i[1] for i in word[1][1]}))
-    # DEBUG: tf_idf_merged.saveAsTextFile('tf_idf')
 
+    #sortByKey function maintains the order of elements. 
+    #It receives key-value pairs (K, V) as an input, sorts the elements in 
+    #ascending or descending order and generates a dataset in an order.
+    
     sorted_tf_idf = tf_idf_merged.sortByKey()
     q = sorted_tf_idf.lookup(QUERY)
 
     q = [i for i in q]
     q_norm = sum(map(lambda x: x ** 2, q[0].values())) ** (1/2)
 
+    # Apply our formula to retrieve the term - term frequency
     similartities = tf_idf_merged.map(lambda w: (w[0], sum([q[0][elem] * w[1][elem] for elem in q[0].keys() & w[1].keys()]) / (sum(map(lambda x: x ** 2, w[1].values())) ** (1/2) * q_norm)))
+      
+    # take top 5 from ordered the pair in descending order
+    terms = similartities.takeOrdered(6, key=lambda word: -word[1])
 
-    # take top 20 from ordered the pair in descending order
-    terms = similartities.takeOrdered(21, key=lambda word: -word[1])
-
-    output.write(f'\nTop 20 similar to {QUERY}:\n')
-    # query should match itself (~1.0 score), so we skip that
-    output.writelines([f'{word} {item}\n' for (word, item) in terms if word != QUERY])
+    output.write(f'\nTop 5 similar to {QUERY}:\n')
+    # We skip the query term because that matches itself (~1.0 score)
+    output.writelines([f'|    {word} -------- {item}   |\n' for (word, item) in terms if word != QUERY])
     output.close()
